17d536d147a00b924dfb6ae9c5bdfd2323548a0f
scullionw <scuw1801@usherbrooke.ca>
2018-11-03 16:51:40 -0400

    debug working

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,30 @@ struct ResponseMapper {
     map: Mutex<HashMap<String, Sender<String>>>,
 }
 
-fn send(message: String) {
+fn send_to_node(message: String) {
     let mut stream = TcpStream::connect("127.0.0.1:8100").unwrap();
-    stream.write(message.as_bytes()).unwrap();
+    println!("SENDING STRING: {:?}", message);
+    let message = message.as_bytes();
+    println!("SENDING BYTES: {:?}", message);
+    stream.write(message).unwrap();
     stream.flush().unwrap();
 }
 
 #[get("/<sensorid>/<message>")]
-fn read_sensor(sensorid: String, message: String, state: State<ResponseMapper>) -> String {
+fn read_sensor(sensorid: String, message: String, state: State<Arc<ResponseMapper>>) -> String {
     let (tx, rx) = channel();
     {
         let mut map = state.map.lock().unwrap();
-        map.insert(sensorid.clone(), tx);
+        map.insert(message.clone(), tx);
     }
-    send(message);
+    send_to_node(message);
     let response = rx.recv().unwrap();
     format!("Message received from {}: {}!", sensorid, response)
+}
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello world!"
 }
 
 fn main() {
@@ -40,13 +48,14 @@ fn main() {
     });
     let response_mapper_rocket = response_mapper_mbed.clone();
     thread::spawn(move || {
-        let listener = TcpListener::bind("127.0.0.1:8100").unwrap();
+        let listener = TcpListener::bind("127.0.0.1:8200").unwrap();
         for stream in listener.incoming() {
             let mut stream = stream.unwrap();
             let mut buf = [0; BUF_SIZE];
-            stream.read(&mut buf).unwrap();
-            let data = String::from_utf8_lossy(&buf).to_string();
-            println!("{}", data);
+            let bytes_read = stream.read(&mut buf).unwrap();
+            println!("RECEIVED BUF: {:?}", &buf[..bytes_read]);
+            let data = String::from_utf8_lossy(&buf[..bytes_read]).to_string();
+            println!("RECEIVED STRING: {:?}", data);
             response_mapper_mbed
                 .map
                 .lock()
@@ -57,7 +66,7 @@ fn main() {
     });
 
     rocket::ignite()
-        .mount("/", routes![read_sensor])
+        .mount("/", routes![read_sensor, hello])
         .manage(response_mapper_rocket)
         .launch();
 }
