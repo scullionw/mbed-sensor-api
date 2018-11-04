0790853666cde5ec30529b6afd755c26d48658d8
scullionw <scuw1801@usherbrooke.ca>
2018-11-04 00:05:35 -0400

    json serde working

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@
 mod comms;
 mod sensors;
 
+use crate::sensors::{RequestType, Sensor, SensorMessage, SensorType};
+use rocket::request::Form;
 use rocket::{get, routes, State};
+use rocket_contrib::json::Json;
 use serde_derive::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::{channel, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -13,18 +16,30 @@ use std::thread;
 const NODE_ADDR: &str = "127.0.0.1:8100";
 const LISTENER_ADDR: &str = "127.0.0.1:8200";
 
-type ResponseMap = Arc<Mutex<HashMap<String, Sender<String>>>>;
+type ResponseMap = Arc<Mutex<HashMap<u32, Sender<String>>>>;
+type SensorList = Arc<Mutex<HashSet<Sensor>>>;
 
-#[get("/<sensorid>/<message>")]
-fn sensor_test(sensorid: String, message: String, map: State<ResponseMap>) -> String {
+#[get("/sensor?<sensor..>")]
+fn read_sensor(sensor: Form<Sensor>, map: State<ResponseMap>) -> String {
+    let sensor = sensor.into_inner();
     let (tx, rx) = channel();
     {
         let mut map = map.lock().unwrap();
-        map.insert(message.clone(), tx);
+        map.insert(sensor.sensor_id, tx);
     }
-    comms::send_to_node(NODE_ADDR, message);
+    let sensor_message = SensorMessage {
+        sensor,
+        request_type: RequestType::Get,
+        payload: String::new(),
+    };
+    comms::send_to_node(NODE_ADDR, serde_json::to_string(&sensor_message).unwrap());
     let response = rx.recv().unwrap();
-    format!("Message received from {}: {}!", sensorid, response)
+    format!("Message received from {}: {}!", sensor.sensor_id, response)
+}
+
+#[get("/sensors", format = "json")]
+fn sensors(sensor_list: State<SensorList>) -> Json<SensorList> {
+    Json((*sensor_list).clone())
 }
 
 #[get("/hello")]
@@ -36,10 +51,30 @@ fn main() {
     let response_map = Arc::new(Mutex::new(HashMap::new()));
     let (rocket_map, mbed_map) = (response_map.clone(), response_map);
 
+    let sensor_1 = Sensor {
+        sensor_id: 1,
+        sensor_type: SensorType::Light,
+    };
+    let sensor_2 = Sensor {
+        sensor_id: 2,
+        sensor_type: SensorType::Lock,
+    };
+
+    let sensor_list = Arc::new(Mutex::new(HashSet::new()));
+    {
+        let mut sensor_list = sensor_list.lock().unwrap();
+        sensor_list.insert(sensor_1);
+        sensor_list.insert(sensor_2);
+        // println!("{:#?}", sensor_list);
+        let sensor_list_json = serde_json::to_string(&*sensor_list).unwrap();
+        println!("{}", sensor_list_json);
+    }
+
     thread::spawn(move || comms::node_listener(LISTENER_ADDR, mbed_map));
 
     rocket::ignite()
-        .mount("/", routes![hello, sensor_test])
+        .mount("/", routes![hello, sensors, read_sensor])
         .manage(rocket_map)
+        .manage(sensor_list)
         .launch();
 }
--- src/sensors.rs
+++ src/sensors.rs
@@ -1,27 +1,28 @@
+use rocket::{FromForm, FromFormValue};
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Sensor {
-    sensor_id: u32,
-    sensor_type: SensorType,
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, FromForm, Copy, Clone)]
+pub struct Sensor {
+    pub sensor_id: u32,
+    pub sensor_type: SensorType,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct SensorMessage {
-    sensor: Sensor,
-    request_type: RequestType,
-    payload: String,
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Clone)]
+pub struct SensorMessage {
+    pub sensor: Sensor,
+    pub request_type: RequestType,
+    pub payload: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-enum RequestType {
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Copy, Clone)]
+pub enum RequestType {
     Get,
     GetResponse,
     Set,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-enum SensorType {
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, FromFormValue, Copy, Clone)]
+pub enum SensorType {
     Thermometer,
     Light,
     SmartSwitch,
